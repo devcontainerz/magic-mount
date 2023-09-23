a87244bd281b2ee1afb5fcce63a9a5131cf5e34d
Sebastian Steins <592313+sebst@users.noreply.github.com>
2023-09-23 16:03:41 +0200

    Install feature to running container

---
+++ examples/install-feature-to-container.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+if [[ "${TRACE-0}" == "1" ]]; then
+	set -o xtrace
+fi
+
+if [[ "${1-}" =~ ^-*h(elp)?$ ]]; then
+	echo 'Usage: ./mount-to-container.sh <container> <featurepath>
+
+Installs a feature to a running container.
+
+'
+	exit
+fi
+
+cd "$(dirname "$0")"
+
+main() {
+	local CONTAINER=$1
+	local FEATUREPATH=$2
+	echo "Installing feature ${FEATUREPATH} to container ${CONTAINER}"
+	mount-to-container.sh $CONTAINER $FEATUREPATH /featureInstall
+	docker exec -d $CONTAINER "/featureInstall/install.sh"
+}
+
+
+main "$@"
