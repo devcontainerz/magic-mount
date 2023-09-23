411ca6588d90167ace02bb6133c4f9ee862796ad
Sebastian Steins <592313+sebst@users.noreply.github.com>
2023-09-23 13:45:23 +0200

    Add: RelPath

--- _run_all.sh
+++ _run_all.sh
@@ -2,12 +2,12 @@
 
 
 
-00-prereq.sh
-01-install-docker.sh
-02-make-container.sh
-03-create-mnt-in-container.sh
-04-magic-mount.sh
-05-enter-interactive-shell.sh
+./00-prereq.sh
+./01-install-docker.sh
+./02-make-container.sh
+./03-create-mnt-in-container.sh
+./04-magic-mount.sh
+./05-enter-interactive-shell.sh
 
 
 echo "Fin."
