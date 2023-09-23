7bbaf2eed575bd69b8f3f326789f6bc344a0fe33
Sebastian Steins <592313+sebst@users.noreply.github.com>
2023-09-23 14:49:30 +0200

    test...

--- test.sh
+++ test.sh
@@ -20,11 +20,11 @@ done < /proc/self/mountinfo
 SUBPATH=$(echo $REALPATH | sed s,^$FILESYS,,)
 DEVDEC=$(printf "%d %d" $(stat --format "0x%t 0x%T" $DEV))
 
-docker-enter $CONTAINER -- sh -c \
+docker-enter $CONTAINER sh -c \
 	     "[ -b $DEV ] || mknod --mode 0600 $DEV b $DEVDEC"
-docker-enter $CONTAINER -- mkdir /tmpmnt
-docker-enter $CONTAINER -- mount $DEV /tmpmnt
-docker-enter $CONTAINER -- mkdir -p $CONTPATH
-docker-enter $CONTAINER -- mount -o bind /tmpmnt/$SUBROOT/$SUBPATH $CONTPATH
-docker-enter $CONTAINER -- umount /tmpmnt
-docker-enter $CONTAINER -- rmdir /tmpmnt
+docker-enter $CONTAINER mkdir /tmpmnt
+docker-enter $CONTAINER mount $DEV /tmpmnt
+docker-enter $CONTAINER mkdir -p $CONTPATH
+docker-enter $CONTAINER mount -o bind /tmpmnt/$SUBROOT/$SUBPATH $CONTPATH
+docker-enter $CONTAINER umount /tmpmnt
+docker-enter $CONTAINER rmdir /tmpmnt
